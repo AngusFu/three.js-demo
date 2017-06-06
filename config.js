db92c63e32f32da2d974161b898ec36400897cf0
wemlion <angusfu1126@qq.com>
2017-06-07 00:05:03 +0800

    commit

---
+++ config.js
@@ -0,0 +1,31 @@
+export const screenOneFlags = [{
+  en: "Technology driven retail",
+  cn: "技术驱动零售",
+  cord: "10x30",
+  maxs: 0.6,
+  mins: 0.2
+}, {
+  en: "New retail",
+  cn: "新零售",
+  cord: "24x40",
+  maxs: 1.5,
+  mins: 1
+}, {
+  en: "Digital marketing",
+  cn: "数字营销",
+  cord: "28x32",
+  maxs: 0.6,
+  mins: 0.2
+}, {
+  en: "Reconstruction of freight yard",
+  cn: "人货场重构",
+  cord: "38x24",
+  maxs: 0.5,
+  mins: 0.15
+}, {
+  en: "Data brand",
+  cn: "数据赋能品牌",
+  cord: "33x38",
+  maxs: 1.2,
+  mins: 0.9
+}];
--- index.js
+++ index.js
@@ -1,37 +1,53 @@
+import raf from 'raf';
+import now from 'performance-now';
+import { screenOneFlags } from './config';
 const THREE = window.THREE;
-const div = document.getElementById('test');
 
-const SEPARATION = 100;
-const AMOUNTX = 30;
+const SEPARATION = 70;
+const AMOUNTX = 50;
 const AMOUNTY = 60;
-
+window.x = (x) => mouseX = x;
+window.y = (x) => mouseY = x;
 let count = 0;
 let mouseX = 85;
-let mouseY = -342;
+let mouseY = -420;
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
 
-const flagPosMap = {
-  '14x39': 1
-};
+const screenOneFlagStyles = screenOneFlags.map(f => {
+  const { cn, en, mins, maxs } = f;
+  const div = document.createElement('div');
+  div.className = 'flag-1';
+  div.innerHTML = `<p>${en}</p><h2>${cn}</h2>`;
+  document.body.appendChild(div);
+  return {
+    style: div.style,
+    mins,
+    maxs
+  };
+});
+const screenOneFlagMap = screenOneFlags.reduce((acc, f, i) => {
+  acc[f.cord] = screenOneFlagStyles[i]
+  return acc;
+}, {});
+
+const scene = new THREE.Scene();
+const renderer = new THREE.CanvasRenderer();
+const camera = new THREE.PerspectiveCamera(120, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.z = 800;
 
 const {
   particles,
   pointsWithFlag
 } = initParticles(); 
-
-const scene = new THREE.Scene();
-const renderer = new THREE.CanvasRenderer();
-const camera = new THREE.PerspectiveCamera(120, window.innerWidth / window.innerHeight, 1, 10000);
-camera.position.z = 1000;
 particles.forEach(p => scene.add(p));
 
 /**
  * Axis helper
  */
-const axisHelper = new THREE.AxisHelper(1000000);
-axisHelper.position.set(0, 0, 0);
-scene.add(axisHelper);
+// const axisHelper = new THREE.AxisHelper(1000000);
+// axisHelper.position.set(0, 0, 0);
+// scene.add(axisHelper);
 
 init();
 
@@ -53,7 +69,7 @@ function init() {
 }
 
 function animate() {
-  requestAnimationFrame(animate);
+  raf(animate);
   render();
 }
 
@@ -62,20 +78,30 @@ function render() {
   camera.position.y += (-mouseY - camera.position.y) * .05;
   camera.lookAt(scene.position);
 
-  var i = 0;
-  var j = 0;
-  for (var ix = 0; ix < AMOUNTX; ix++) {
-    for (var iy = 0; iy < AMOUNTY; iy++) {
-      let particle = particles[i++];
+  let i = 0;
+  let j = 0;
+  for (let ix = 0; ix < AMOUNTX; ix++) {
+    for (let iy = 0; iy < AMOUNTY; iy++) {
+      const particle = particles[i++];
       particle.position.y = (Math.sin((ix + count) * 0.3) * 50) + (Math.sin((iy + count) * 0.5) * 50);
-      particle.scale.x = particle.scale.y = (Math.sin((ix + count) * 0.3) + 1) * 2 + (Math.sin((iy + count) * 0.5) + 1) * 2;
+      particle.scale.x
+        = particle.scale.y
+        = (Math.sin((ix + count) * 0.3) + 1) * 1.4 + (Math.sin((iy + count) * 0.5) + 1) * 1.4;
 
-      if (`${ix}x${iy}` in flagPosMap) {
-        var point = pointsWithFlag[j++];
+      if (screenOneFlagMap[`${ix}x${iy}`]) {
+        const point = pointsWithFlag[j++];
         point.y = particle.position.y;
+
+        const { style, mins, maxs } = screenOneFlagMap[`${ix}x${iy}`];
         const { x, y } = toScreenCords(point, camera);
-        const scale = Math.min(Math.max(particle.scale.x / 4, .5), 2);
-        div.style.transform = `translate3d(${x}px, ${y}px, 0) scale(${scale})`;
+        const scale = Math.min(
+          Math.max(
+            particle.scale.x / 6 * maxs,
+            mins
+          ),
+          maxs
+        ) + particle.scale.x / 30;
+        style.transform = `translate3d(${x}px, ${y}px, 0) scale(${scale})`;
       }
     }
   }
@@ -95,21 +121,21 @@ function initParticles() {
     color: 0x94fffb,
     program: function (context) {
       context.beginPath();
-      context.arc(0, 0, 0.6, 0, Math.PI * 2, true);
+      context.arc(0, 0, 1, 0, Math.PI * 2, true);
       context.fill();
     }
   });
 
   for (let i = 0; i < AMOUNTX; i++) {
     for (let j = 0; j < AMOUNTY; j++) {
-      let particle = new THREE.Sprite(material);
+      const particle = new THREE.Sprite(material);
       particle.position.x = i * SEPARATION - ((AMOUNTX * SEPARATION) / 2);
       particle.position.z = j * SEPARATION - ((AMOUNTY * SEPARATION) / 2);
 
       particles[index++] = particle;
       // scene.add(particle);
 
-      if (`${i}x${j}` in flagPosMap) {
+      if (`${i}x${j}` in screenOneFlagMap) {
         const { x, z } = particle.position;
         pointsWithFlag.push({ x, z });
       }
@@ -129,20 +155,52 @@ function toScreenCords({
   y,
   z
 }, camera) {
-  var vector = (new THREE.Vector3(x, y, z)).project(camera);
+  const vector = (new THREE.Vector3(x, y, z)).project(camera);
   return {
     x: Math.round(windowHalfX * (1 + vector.x)),
     y: Math.round(windowHalfY * (1 - vector.y))
   };
 }
 
-/*function appendImage(src) {
-  var texture = new THREE.TextureLoader(src);
-  var material = new THREE.MeshBasicMaterial({
-    map: texture,
-    side: THREE.DoubleSide
-  });
-  var gemoetry = new THREE.PlaneGeometry(50, 50, 1, 1);
-  var mesh = new THREE.Mesh(gemoetry, material);
-  return mesh;
-}*/
+function makeDigits() {
+  const canvas = document.createElement('canvas');
+  canvas.width = 20;
+  canvas.height = 300;
+  const ctx = canvas.getContext('2d');
+
+  const fontSize = 12;
+  const column = Math.floor(canvas.width / fontSize) / 2;
+  let www = 0;
+
+  raf(draw);
+
+  //输出文字
+  let last = now();
+  function draw(t) {
+    if (now() - last > 100) {
+      last = t;
+      ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+      ctx.fillRect(0,0,canvas.width,canvas.height);
+      ctx.fillStyle = "#01c7bf";
+      ctx.font = fontSize + "px arial";
+      ctx.fillText(
+        Math.random() > .4 ? 1 : 0,
+        5,
+        www * fontSize + 10
+      );
+
+      www = www * fontSize > canvas.height ? 0 : www;
+      www += 2;
+    }
+    raf(draw);
+  }
+
+  document.body.appendChild(canvas);
+  canvas.style.cssText = `
+    position: absolute;
+    z-index: 9;
+    left: 30px;
+    top: 60px;
+  `;
+}
+makeDigits();
